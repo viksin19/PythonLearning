981265da00eb39d43c7510da132ca110983f84cc
Singh, Vikram (vs192b) <vs192b@att.com>
2020-06-21 13:36:27 +0530

    [vikram] Variables

---
+++ Variable/Variable.py
@@ -0,0 +1,33 @@
+# integer
+num = 3
+print(num)  # output : 3
+# float
+num2 = 4.5
+print(num2)  # output : 4.5
+
+# Can be in double quotes
+word = 'String Variable'
+print(word)  # output : String Variable
+
+# touple
+touple = ('1', 2, '3', 4, 'Hello world', (1, 2, 3))
+print(touple)  # output : ('1', 2, '3', 4, 'Hello world', (1, 2, 3))
+
+# List
+List = [1, 2, 3, 5, '7', '8', [3, 4, 5]]
+print(List)  # output : [1, 2, 3, 5, '7', '8', [3, 4, 5]]
+
+# Set
+Set = {1, 2, 4, 5, '5', '6'}
+print(Set)  # output : { 1,2,4,5,'5','6'}
+# Set2 = { 1,2,4,5,'5','6',{1,2,3}}
+# print(Set2) #output : TypeError: unhashable type: 'set'
+
+# Boolean
+Boolean = True
+print(Boolean)  # output : True
+print(not(Boolean))  # output : False
+
+# Dictionary  = { key : Value } -- JSON
+Dictionary = { "Apple" : 10 , "Oranges" : 20 }
+print(Dictionary) #output : {'Apple': 10, 'Oranges': 20}
