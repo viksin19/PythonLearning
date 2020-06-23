51a0a2daa59cda22eca293b0972701f1959d31f9
Singh, Vikram (vs192b) <vs192b@att.com>
2020-06-23 11:13:35 +0530

    [vikram] some challenges from IBM

---
+++ Variable/Ibm.py
@@ -0,0 +1,17 @@
+name = 'Michel Jackson'
+print(name.find('el'))
+A = {'x','b'}
+A.add('c')
+print(A)
+A.add('z')
+print(A)
+
+A = {'A','B','C','A'}
+A.add('C')
+print(A)
+
+L = ['1','2','3']
+i = L.index('2')
+print(i)
+L.remove('2')
+print(L)
